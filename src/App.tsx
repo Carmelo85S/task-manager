2373c285e1613fe6ffb817a5add37bd65d884621
Carmelo85S <carmelo.salis85@gmail.com>
2025-02-03 12:00:03 +0100

    mark as complete and change bg color

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 const App = () => {
 
   //declaring task list and input field
-  const [tasks, setTasks] = useState<string[]>([]);
+  const [tasks, setTasks] = useState<{task:string, completed: boolean}[]>([]);
   const [inputTask, setInputTask] = useState<string>("");
 
   //setInputTask take the insert value from the input
@@ -15,8 +15,20 @@ const App = () => {
   const handleFormSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     if (inputTask.trim() === "") return;
-    setTasks([...tasks, inputTask]); 
+    setTasks([...tasks, {task: inputTask, completed: false }]); 
     setInputTask(""); 
+  };
+
+  // Remove specific task
+  const handleDelete = (toDelete: string) => {
+    setTasks(tasks.filter((task) => task.task !== toDelete));
+  };
+
+  // Handle task completion toggle
+  const handleCheckboxChange = (index: number) => {
+    const updatedTasks = [...tasks];
+    updatedTasks[index].completed = !updatedTasks[index].completed;
+    setTasks(updatedTasks);
   };
 
   return (
@@ -39,7 +51,27 @@ const App = () => {
       {tasks.length > 0 && (
         <ul>
           {tasks.map((task, index) => (
-            <li key={index}>{task}</li>
+            <li 
+              key={index}
+              style={{
+                backgroundColor: task.completed ? "green" : "transparent",
+                color: task.completed ? "white" : "black",
+                padding: "5px",
+                marginBottom: "5px",
+                borderRadius: "4px",
+              }}
+            >
+              {task.task}
+              <button onClick={() => handleDelete(task.task)}>Delete</button>
+              <button onClick={() => handleDelete(task.task)}>Edit</button>
+              <input 
+                type="checkbox" 
+                name="completed" 
+                id="completed"
+                checked={task.completed}
+                onChange={() => handleCheckboxChange(index)}
+              />
+            </li>
           ))}
         </ul>
       )}
