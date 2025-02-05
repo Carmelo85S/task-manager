9d60ccd75c0939ddfaf28cfc11f9a3741ce33bcb
Carmelo85S <carmelo.salis85@gmail.com>
2025-02-05 09:20:22 +0100

    changed name functions

--- src/App.js
+++ src/App.js
@@ -17,65 +17,52 @@ function _arrayLikeToArray(r, a) { (null == a || a > r.length) && (a = r.length)
 function _iterableToArrayLimit(r, l) { var t = null == r ? null : "undefined" != typeof Symbol && r[Symbol.iterator] || r["@@iterator"]; if (null != t) { var e, n, i, u, a = [], f = !0, o = !1; try { if (i = (t = t.call(r)).next, 0 === l) { if (Object(t) !== t) return; f = !1; } else for (; !(f = (e = i.call(t)).done) && (a.push(e.value), a.length !== l); f = !0); } catch (r) { o = !0, n = r; } finally { try { if (!f && null != t.return && (u = t.return(), Object(u) !== u)) return; } finally { if (o) throw n; } } return a; } }
 function _arrayWithHoles(r) { if (Array.isArray(r)) return r; }
 var App = function App() {
-  // Declaring task list and input field
   var _useState = (0, _react.useState)([]),
     _useState2 = _slicedToArray(_useState, 2),
     tasks = _useState2[0],
     setTasks = _useState2[1];
   var _useState3 = (0, _react.useState)(""),
     _useState4 = _slicedToArray(_useState3, 2),
-    inputTask = _useState4[0],
-    setInputTask = _useState4[1];
+    newTask = _useState4[0],
+    setNewTask = _useState4[1];
   var _useState5 = (0, _react.useState)(null),
     _useState6 = _slicedToArray(_useState5, 2),
-    editingIndex = _useState6[0],
-    setEditingIndex = _useState6[1];
+    taskEdit = _useState6[0],
+    setTaskEdit = _useState6[1];
   var _useState7 = (0, _react.useState)(""),
     _useState8 = _slicedToArray(_useState7, 2),
-    editingTask = _useState8[0],
-    setEditingTask = _useState8[1];
-
-  // Handle input for new task
-  var handleInput = function handleInput(e) {
-    setInputTask(e.target.value);
+    editedTaskText = _useState8[0],
+    setEditedTaskText = _useState8[1];
+  var handleNewTaskChange = function handleNewTaskChange(e) {
+    setNewTask(e.target.value);
   };
-
-  // Prevent empty task, add task to array, and clean the input
-  var handleFormSubmit = function handleFormSubmit(e) {
+  var handleTaskSubmit = function handleTaskSubmit(e) {
     e.preventDefault();
-    if (inputTask.trim() === "") return;
+    if (newTask.trim() === "") return;
     setTasks([].concat(_toConsumableArray(tasks), [{
-      task: inputTask,
+      task: newTask,
       completed: false
     }]));
-    setInputTask("");
+    setNewTask("");
   };
-
-  // Remove specific task
-  var handleDelete = function handleDelete(toDelete) {
+  var removeTask = function removeTask(taskToRemove) {
     setTasks(tasks.filter(function (task) {
-      return task.task !== toDelete;
+      return task.task !== taskToRemove;
     }));
   };
-
-  // Handle task edit mode
-  var handleEdit = function handleEdit(index) {
-    setEditingIndex(index);
-    setEditingTask(tasks[index].task);
+  var startEditingTask = function startEditingTask(index) {
+    setTaskEdit(index);
+    setEditedTaskText(tasks[index].task);
   };
-
-  // Save the edited task
-  var handleSaveEdit = function handleSaveEdit() {
-    if (editingTask.trim() === "") return;
+  var saveEditedTask = function saveEditedTask() {
+    if (editedTaskText.trim() === "") return;
     var updatedTasks = _toConsumableArray(tasks);
-    updatedTasks[editingIndex].task = editingTask;
+    updatedTasks[taskEdit].task = editedTaskText;
     setTasks(updatedTasks);
-    setEditingIndex(null);
-    setEditingTask("");
+    setTaskEdit(null);
+    setEditedTaskText("");
   };
-
-  // Handle task completion toggle
-  var handleCheckboxChange = function handleCheckboxChange(index) {
+  var toggleTask = function toggleTask(index) {
     var updatedTasks = _toConsumableArray(tasks);
     updatedTasks[index].completed = !updatedTasks[index].completed;
     setTasks(updatedTasks);
@@ -86,19 +73,19 @@ var App = function App() {
     className: "heading"
   }, "Lazy List"), /*#__PURE__*/React.createElement("p", {
     className: "quote"
-  }, "Get things done...or at least pretend to")), /*#__PURE__*/React.createElement("form", {
-    onSubmit: handleFormSubmit
+  }, "Get things done... or at least pretend to")), /*#__PURE__*/React.createElement("form", {
+    onSubmit: handleTaskSubmit
   }, /*#__PURE__*/React.createElement("input", {
     type: "text",
     name: "task",
     id: "task",
     placeholder: "Insert task",
-    value: inputTask,
-    onChange: handleInput,
+    value: newTask,
+    onChange: handleNewTaskChange,
     required: true
   }), /*#__PURE__*/React.createElement("button", {
     type: "submit"
-  }, "Submit Task")), /*#__PURE__*/React.createElement("div", null, tasks.length > 0 && /*#__PURE__*/React.createElement("ul", {
+  }, "Add Task")), /*#__PURE__*/React.createElement("div", null, tasks.length > 0 && /*#__PURE__*/React.createElement("ul", {
     className: "task-list-container"
   }, tasks.map(function (task, index) {
     return /*#__PURE__*/React.createElement("li", {
@@ -108,23 +95,23 @@ var App = function App() {
         backgroundColor: task.completed ? "green" : "transparent",
         color: task.completed ? "white" : "black"
       }
-    }, editingIndex === index ? /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("input", {
+    }, taskEdit === index ? /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("input", {
       type: "text",
-      value: editingTask,
+      value: editedTaskText,
       onChange: function onChange(e) {
-        return setEditingTask(e.target.value);
+        return setEditedTaskText(e.target.value);
       }
     })) : /*#__PURE__*/React.createElement("span", null, task.task), /*#__PURE__*/React.createElement("div", {
       className: "btn-container"
     }, /*#__PURE__*/React.createElement("button", {
       onClick: function onClick() {
-        return handleEdit(index);
+        return startEditingTask(index);
       }
     }, "Edit"), /*#__PURE__*/React.createElement("button", {
-      onClick: handleSaveEdit
+      onClick: saveEditedTask
     }, "Save"), /*#__PURE__*/React.createElement("button", {
       onClick: function onClick() {
-        return handleDelete(task.task);
+        return removeTask(task.task);
       }
     }, "Delete"), /*#__PURE__*/React.createElement("div", {
       className: "checkbox-container"
@@ -134,7 +121,7 @@ var App = function App() {
       type: "checkbox",
       checked: task.completed,
       onChange: function onChange() {
-        return handleCheckboxChange(index);
+        return toggleTask(index);
       }
     }))));
   }))));
--- src/App.tsx
+++ src/App.tsx
@@ -1,120 +1,110 @@
 import { useState } from "react";
 import './index.css';
+
 const App = () => {
-
-  // Declaring task list and input field
   const [tasks, setTasks] = useState<{ task: string, completed: boolean }[]>([]);
-  const [inputTask, setInputTask] = useState<string>("");
-  const [editingIndex, setEditingIndex] = useState<number | null>(null);
-  const [editingTask, setEditingTask] = useState<string>("");
+  const [newTask, setNewTask] = useState<string>("");
+  const [taskEdit, setTaskEdit] = useState<number | null>(null);
+  const [editedTaskText, setEditedTaskText] = useState<string>("");
 
-  // Handle input for new task
-  const handleInput = (e: React.ChangeEvent<HTMLInputElement>) => {
-      setInputTask(e.target.value);
+  const handleNewTaskChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setNewTask(e.target.value);
   };
 
-  // Prevent empty task, add task to array, and clean the input
-  const handleFormSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-      e.preventDefault();
-      if (inputTask.trim() === "") return;
-      setTasks([...tasks, { task: inputTask, completed: false }]);
-      setInputTask("");
+  const handleTaskSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (newTask.trim() === "") return;
+    setTasks([...tasks, { task: newTask, completed: false }]);
+    setNewTask("");
   };
 
-  // Remove specific task
-  const handleDelete = (toDelete: string) => {
-      setTasks(tasks.filter((task) => task.task !== toDelete));
+  const removeTask = (taskToRemove: string) => {
+    setTasks(tasks.filter(task => task.task !== taskToRemove));
   };
 
-  // Handle task edit mode
-  const handleEdit = (index: number) => {
-      setEditingIndex(index);
-      setEditingTask(tasks[index].task);
+  const startEditingTask = (index: number) => {
+    setTaskEdit(index);
+    setEditedTaskText(tasks[index].task);
   };
 
-  // Save the edited task
-  const handleSaveEdit = () => {
-      if (editingTask.trim() === "") return;
-      const updatedTasks = [...tasks];
-      updatedTasks[editingIndex!].task = editingTask;
-      setTasks(updatedTasks);
-      setEditingIndex(null);
-      setEditingTask("");
+  const saveEditedTask = () => {
+    if (editedTaskText.trim() === "") return;
+    const updatedTasks = [...tasks];
+    updatedTasks[taskEdit!].task = editedTaskText;
+    setTasks(updatedTasks);
+    setTaskEdit(null);
+    setEditedTaskText("");
   };
 
-  // Handle task completion toggle
-  const handleCheckboxChange = (index: number) => {
-      const updatedTasks = [...tasks];
-      updatedTasks[index].completed = !updatedTasks[index].completed;
-      setTasks(updatedTasks);
+  const toggleTask = (index: number) => {
+    const updatedTasks = [...tasks];
+    updatedTasks[index].completed = !updatedTasks[index].completed;
+    setTasks(updatedTasks);
   };
 
   return (
     <main>
-        <div className="title-container">
-          <h1 className="heading">
-            Lazy List
-          </h1>
-          <p className="quote">
-            Get things done...or at least pretend to
-          </p>
-        </div>
-        <form onSubmit={handleFormSubmit}>
-          <input
-            type="text"
-            name="task"
-            id="task"
-            placeholder="Insert task"
-            value={inputTask}
-            onChange={handleInput}
-            required
-          />
-          <button type="submit">Submit Task</button>
-        </form>
+      <div className="title-container">
+        <h1 className="heading">Lazy List</h1>
+        <p className="quote">Get things done... or at least pretend to</p>
+      </div>
+      
+      <form onSubmit={handleTaskSubmit}>
+        <input
+          type="text"
+          name="task"
+          id="task"
+          placeholder="Insert task"
+          value={newTask}
+          onChange={handleNewTaskChange}
+          required
+        />
+        <button type="submit">Add Task</button>
+      </form>
 
-        <div>
-          {/* Render the task list */}
-          {tasks.length > 0 && (
-            <ul className="task-list-container">
-              {tasks.map((task, index) => (
-                <li
-                  className="task"
-                  key={index}
-                  style={{
-                    backgroundColor: task.completed ? "green" : "transparent",
-                    color: task.completed ? "white" : "black"
-                  }}
-                >
-                  {editingIndex === index ? (
-                    <div>
-                      <input
-                        type="text"
-                        value={editingTask}
-                        onChange={(e) => setEditingTask(e.target.value)}
-                      />
-                    </div>
-                  ) : (
-                    <span>{task.task}</span>
-                  )}
-                  <div className="btn-container">
-                  <button onClick={() => handleEdit(index)}>Edit</button>
-                    <button onClick={handleSaveEdit}>Save</button>
-                    <button onClick={() => handleDelete(task.task)}>Delete</button>
-                    
-                    <div className="checkbox-container">
-                      <label htmlFor="checkbox">Done</label>
-                      <input
-                        type="checkbox"
-                        checked={task.completed}
-                        onChange={() => handleCheckboxChange(index)}
-                      />
-                    </div>
+      <div>
+        {tasks.length > 0 && (
+          <ul className="task-list-container">
+            {tasks.map((task, index) => (
+              <li
+                className="task"
+                key={index}
+                style={{
+                  backgroundColor: task.completed ? "green" : "transparent",
+                  color: task.completed ? "white" : "black",
+                }}
+              >
+                {taskEdit === index ? (
+                  <div>
+                    <input
+                      type="text"
+                      value={editedTaskText}
+                      onChange={(e) => setEditedTaskText(e.target.value)}
+                    />
                   </div>
-                </li>
-              ))}
-            </ul>
-          )}
-        </div>
+                ) : (
+                  <span>{task.task}</span>
+                )}
+
+                <div className="btn-container">
+                  <button onClick={() => startEditingTask(index)}>Edit</button>
+                  <button onClick={saveEditedTask}>Save</button>
+                  <button onClick={() => removeTask(task.task)}>Delete</button>
+
+                  <div className="checkbox-container">
+                    <label htmlFor="checkbox">Done</label>
+                    <input
+                      type="checkbox"
+                      checked={task.completed}
+                      onChange={() => toggleTask(index)}
+                    />
+                  </div>
+                </div>
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
     </main>
   );
 };
